52cb494305d3677b29ef441f068a36955e026314
Jhoni Davinne <jhoni.davinne@hotmail.com>
2025-05-15 02:13:52 -0300

    Adicionado CORS

--- src/app.js
+++ src/app.js
@@ -2,12 +2,29 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import { resolve } from 'path';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
+
+const whiteList = [
+  'https://api.davinne.dev',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 import './database';
 
@@ -20,6 +37,8 @@ class App {
   }
 
   middleswares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
